9cd78e4719ed285147be35fd2fe8bcd266bde365
Robert Riley <riley1969@gmail.com>
2025-02-18 22:03:43 -0500

    Test branch

---
+++ my_first_script.py
@@ -0,0 +1,73 @@
+
+## Outputs Hello World! to the screen
+# print("Hello World!")
+
+# print(1)
+# print(100)
+
+## variables and comments
+# food = "apple"
+# print(food)
+## # this variable stores the cost of the apple
+# cost = 2.75
+# print(cost)
+
+# print(5 + 3)
+# print(5 > 3)
+# print(True or False)
+# food = "burger" + "bun"
+# print(food)
+# print("burger" == "bun")
+# print("bun" == "bun")
+# print("bun" == "Bun")
+
+## #string concatination
+# food = "pan" + "cake"
+# print(food)
+
+# my_fav_food = "Pizza"
+# print (f"My favorite food is {my_fav_food}")
+
+# store = "walmart"
+# print(store.upper())
+# print(store.lower())
+# print(store.capitalize())
+# message = "I shop at the store"
+# print(message.replace("the store", "Walmart"))
+# favourite_food = input("What is your favourite food? ")
+# print(favourite_food)
+
+## #type casting
+# num = int(10)
+# print(num)
+# pi = 3.14
+# print(int(pi))
+# print(float(pi))
+
+# float_result = 10 + 7
+# string_result = "10" + "7"
+# print(float_result)
+# print(string_result)
+# type_error_result = (7.5 + 7.5)
+# print(type_error_result)
+
+## Day 2 Exercise 1
+milk = 4.79
+bread = 3.47
+eggs = 4.99
+cost = milk + bread + eggs
+print(f"Your total is ${cost}")
+
+## Exercise 2
+#Ask user for input and store in variable fav_store
+fav_store = input ("Enter the name of your favourite Grocery Store ")
+#Store the welcome to and the store greeting
+greet = "welcome to " + fav_store
+#Print the greeting by Capitalizing the first letter
+print(greet.title()) 
+
+# Exercise 3
+milk = 3
+bread = 2.5
+total = float(milk) + bread
+print("The total cost is: $" + str(total))
